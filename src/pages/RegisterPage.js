0b69bd8343c973b686910a0630d5078829b13f06
maurixavi <mxavierarevalo@gmail.com>
2024-05-28 02:44:36 -0300

    Update RegisterPage

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,9 +1,37 @@
+import { useState } from "react"
+
 export default function RegisterPage() {
+	const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
+
+	async function register(e){
+		e.preventDefault();
+		const response = await fetch('http://localhost:4000/register', {
+			method: 'POST',
+			body: JSON.stringify({username, password}),
+			headers: {'Content-Type':'application/json'},
+		})
+		
+		if (response.status === 200){
+			alert('registration successful')
+		} else {
+			alert('registration failed')
+		}
+	}
+
 	return (
-		<form className="register">
+		<form className="register" onSubmit={register}>
 			<h1>Register</h1>
-			<input type="text" placeholder="Username" />
-			<input type="password" placeholder="Password" />
+			<input 
+				type="text" 
+				placeholder="Username"
+				value={username}
+				onChange={e => setUsername(e.target.value)} />
+			<input 
+				type="password" 
+				placeholder="Password" 
+				value={password}
+				onChange={e => setPassword(e.target.value)} />
 			<button>Register </button>
 		</form>
 	)
