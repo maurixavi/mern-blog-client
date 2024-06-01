76d085190f8848573b4918095fae387db1a06398
maurixavi <mxavierarevalo@gmail.com>
2024-06-01 01:57:29 -0300

    Update routes

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,60 +1,54 @@
-import { useState, useContext } from "react";
-import { Navigate } from "react-router-dom";
-import { UserContext } from '../UserContext';
-import axios from 'axios';
+import { useState, useContext } from "react"
+import { Navigate } from "react-router-dom"
+import { UserContext } from '../UserContext'
 
 export default function LoginPage() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [redirect, setRedirect] = useState(false);
-  const { setUserInfo } = useContext(UserContext);
+	const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
+	const [redirect, setRedirect] = useState(false)
+	const {setUserInfo} = useContext(UserContext)
 
-  async function login(e) {
-    e.preventDefault();
-    try {
-      const response = await axios.post('https://mern-blog-api-eight.vercel.app/login', { 
-				username, password 
-			});
+	async function login(e){
+		e.preventDefault();
+		const response = await fetch('https://mern-blog-api-eight.vercel.app//login', {
+			method: 'POST',
+			mode: 'cors',
+			body: JSON.stringify({username, password}),
+			headers: {'Content-Type':'application/json'},
+			credentials: 'include', //Always send user credentials (cookies, basic http auth, etc..), even for cross-origin calls.
+		})
+		
+		if (response.ok){
+			alert('login successful')
+			response.json().then(userInfo => {
+				setUserInfo(userInfo)
+				setRedirect(true)
+			})
+			
+		} else {
+			alert('login failed')
+		}
 
-			console.log(response.data)
-      if (response.data.token) {
-				console.log(response.data)
-        localStorage.setItem('token', response.data.token);
-        setUserInfo({
-          id: response.data.id,
-          username: response.data.username
-        });
-        setRedirect(true);
-        alert('Login successful');
-      } else {
-        alert('Login failed');
-      }
-    } catch (error) {
-      console.error('Error during login:', error);
-      //alert('Login failed');
-    }
-  }
+	}
 
-  if (redirect) {
-    return <Navigate to={'/'} />;
-  }
+	if (redirect) {
+		return <Navigate to={'/'} />
+	}
 
-  return (
-    <form className="login" onSubmit={login}>
-      <h1>Login</h1>
-      <input 
-        type="text" 
-        placeholder="Username"
-        value={username}
-        onChange={e => setUsername(e.target.value)} 
-      />
-      <input 
-        type="password" 
-        placeholder="Password" 
-        value={password}
-        onChange={e => setPassword(e.target.value)} 
-      />
-      <button>Login</button>
-    </form>
-  );
+	return (
+		<form className="login" onSubmit={login}>
+			<h1>Login</h1>
+			<input 
+				type="text" 
+				placeholder="Username"
+				value={username}
+				onChange={e => setUsername(e.target.value)} />
+			<input 
+				type="password" 
+				placeholder="Password" 
+				value={password}
+				onChange={e => setPassword(e.target.value)} />
+			<button>Login</button>
+		</form>
+	)
 }
