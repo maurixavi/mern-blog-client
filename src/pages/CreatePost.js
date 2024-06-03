ee2a52521c6d429a557bfb0ae4b2cc69c39005cb
maurixavi <mxavierarevalo@gmail.com>
2024-06-03 01:12:32 -0300

    Add Create Post page

---
+++ src/pages/CreatePost.js
@@ -0,0 +1,77 @@
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
+import { Navigate } from "react-router-dom"
+import { useState } from "react"
+
+const modules = {
+	toolbar: [
+		[{ 'header': [1, 2, false] }],
+		['bold', 'italic', 'underline','strike', 'blockquote'],
+		[{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+		['link', 'image'],
+		['clean']
+	],
+}
+
+const formats = [
+	'header',
+	'bold', 'italic', 'underline', 'strike', 'blockquote',
+	'list', 'bullet', 'indent',
+	'link', 'image'
+]
+
+export default function CreatePost() {
+	const [title, setTitle] = useState('')
+	const [summary, setSummary] = useState('')
+	const [content, setContent] = useState('')
+	const [files, setFiles] = useState('')
+	const [redirect, setRedirect] = useState(false)
+
+	async function createNewPost(e) {
+		const data = new FormData()
+		data.set('title', title)
+		data.set('summary', summary)
+		data.set('content', content)
+		data.set('file', files[0])
+
+		e.preventDefault();
+		const response = await fetch('https://mern-blog-api-eight.vercel.app//post', {
+			method: 'POST',
+			mode: 'cors',
+			body: data,
+			credentials: 'include',
+		})
+		// console.log(await response.json())
+		if (response.ok) {
+			setRedirect(true)
+		}
+	}
+
+	if (redirect) {
+		return <Navigate to={'/'} />
+	}
+
+	return (
+		<form onSubmit={createNewPost}>
+			<input 
+				type="title" 
+				placeholder={'Title'}
+				value={title}
+				onChange={e => setTitle(e.target.value)}/>
+			<input 
+				type="summary" 
+				placeholder={'Summary'}
+				value={summary}
+				onChange={e => setSummary(e.target.value)}/>
+			<input 
+				type="file"	
+				onChange={e => setFiles(e.target.files)}/>
+			<ReactQuill 
+				value={content} 
+				onChange={newValue => setContent(newValue)}
+				modules={modules} 
+				formats={formats} />
+			<button style={{marginTop:'5px'}}>Create Post</button>
+		</form>
+	)
+}
